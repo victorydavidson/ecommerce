85d0de0ed64a0d0e315b832f4bd5dfb6e90cbddc
victorydavidson <victorydavidson19@gmail.com>
2024-07-17 13:11:12 +0100

    an

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,24 @@ import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/product-list" element={<ProductList />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </Router>
-  );
+  try {
+    return (
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    );
+  } catch (error) {
+    console.error("Error rendering App:", error);
+    return <div>Error occurred: {error.message}</div>;
+  }
 };
 
 export default App;
